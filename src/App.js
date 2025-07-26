f0fd8cb1d5e8baf086f1a4544476eccbe8648512
mahenq <90361623+mahenq@users.noreply.github.com>
2025-07-26 23:52:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+import Homepage from "./pages/homepage";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Articles from "./pages/articles";
+import ReadArticle from "./pages/readArticle";
+import Contact from "./pages/contact";
+import Notfound from "./pages/404";
+
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import SubmitForm from "./pages/SubmitForm";
+
+import { TRACKING_ID } from "./data/tracking";
+import "./app.css";
 
 function App() {
+  useEffect(() => {
+    if (TRACKING_ID !== "") {
+      ReactGA.initialize(TRACKING_ID);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* Halaman utama */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/article/:slug" element={<ReadArticle />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
+        <Route path="/submit" element={<SubmitForm />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
     </div>
   );
 }
