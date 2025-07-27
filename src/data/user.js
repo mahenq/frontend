4093be8770d56815ea137117f05c72fe6a54c870
mahenq <90361623+mahenq@users.noreply.github.com>
2025-07-27 23:26:02 +0700

    Fix icon and home

--- src/data/user.js
+++ src/data/user.js
@@ -1,65 +1,69 @@
 const INFO = {
-	main: {
-		title: "Reactfolio by Lia Rahma",
-		name: "Lia Rahma",
-		email: "mail@liarahma",
-		logo: "../logo.png",
-	},
+  main: {
+    title: "Reactfolio by Lia Rahma",
+    name: "Lia Rahma",
+    email: "liarahmaasnaini1010@gmail.com",
+    logo: "../logo.png",
+  },
 
-	socials: {
-		twitter: "https://twitter.com/",
-		github: "https://github.com/",
-		linkedin: "https://linkedin.com/",
-		instagram: "https://instagram.com/",
-		stackoverflow: "https://stackoverflow.com/",
-		facebook: "https://facebook.com/",
-	},
+  socials: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/liarahma10",
+    linkedin: "https://www.linkedin.com/in/liarahma",
+    instagram:
+      "https://www.instagram.com/liarahmaaaa_?igsh=bzJhemI2bmtodXZn&utm_source=qr",
+    stackoverflow: "https://stackoverflow.com/users/31146256/lia-rahma",
+    facebook: "https://facebook.com/",
+  },
 
-	homepage: {
-		title: "Desainer Grafis dan Putri Duyung",
-		description: "Pengen tidur ",
-	},
+  homepage: {
+    title:
+      "Hello, I'm Lia Rahma, a passionate graphic designer turning ideas into visual stories.",
+    description:
+      "I specialize in creating captivating and functional designs, from dynamic branding to expressive digital illustrations. Let's collaborate to bring your brilliant ideas to life!",
+  },
 
-	about: {
-		title: "I’m Lia Rahma.",
-		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
-	},
+  about: {
+    title: "I’m Lia Rahma.",
+    description:
+      "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+  },
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
+  articles: {
+    title:
+      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+    description:
+      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+  },
 
-	projects: [
-		{
-			title: "Project 1",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "/canva.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+  projects: [
+    {
+      title: "Creative Design Replication with Canva",
+      description:
+        "This project showcases my ability to meticulously replicate complex and engaging visual designs using Canva. I focused on detail, accuracy, and aesthetic coherence, demonstrating my strong grasp of design principles and efficiency in leveraging modern design tools to bring concepts to life.",
+      logo: "/canva.png",
+      linkText: "View Project",
+      link: "https://github.com/liarahma10/project_canva",
+    },
 
-		{
-			title: "Project 2",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "/ai.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
+    {
+      title: "Interactive User Interface (UI) Design with Figma",
+      description:
+        "In this project, I crafted an intuitive and engaging User Interface (UI) using Figma. My primary focus was to ensure a superior User Experience (UX) through seamless navigation, clean layouts, and captivating visuals. This highlights my expertise in developing user-centric and highly responsive digital solutions.",
+      logo: "/figma.png",
+      linkText: "View Project",
+      link: "https://github.com/liarahma10/project_figma",
+    },
 
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "cdr.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-	],
+    {
+      title: "CorelDRAW Graphic Design Works",
+      description:
+        "This project demonstrates my proficiency in CorelDRAW to produce original and professional graphic designs. I explored various illustration techniques, compelling layouts, and sophisticated typography to create impactful and memorable visual works. This is a testament to my creativity and advanced technical skills in vector design software.",
+      logo: "cdr.png",
+      linkText: "View Project",
+      link: "https://github.com/liarahma10/project_corel",
+    },
+  ],
 };
 
 export default INFO;
